9f52a66232bcdc6b0166739b475aded6a760d6f1
raja17021998 <rajabhardwaj1989@gmail.com>
2018-09-30 11:29:04 +0530

    Route to add Compenent

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -41,8 +41,9 @@ class Blog extends Component {
                 </header>
                 {/* excat is a boolean value
                 without exact will render all. */}
-                <Route path='/' exact render={()=> <h1>Home</h1>}/>
-                <Route path='/' exact render={()=> <h1>Home 2</h1>}/>
+                {/* <Route path='/' exact render={()=> <h1>Home</h1>}/>
+                <Route path='/' exact render={()=> <h1>Home 2</h1>}/> */}
+                <Route path= '/' exact component={Posts} />
                 
             </div>
         );
